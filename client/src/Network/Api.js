1220b91c060a703377b7f0ca2d8d82f3c8551742
mukul65 <mukul.singh@zentekinfsoft.com>
2023-12-06 12:55:37 +0530

    actions of product

--- client/src/Network/Api.js
+++ client/src/Network/Api.js
@@ -21,4 +21,3 @@ export const getDataId = (id) => API.get(`/auth/getData/${id}`);
 // product operations
 export const getProducts = () => API.get('/product/products');
 export const getProductsById = (id) => API.get(`/product/products/${id}`);
-export const postAddToCart = (formData, quantity, varient) => API.post('/product/products', formData, quantity, varient);
--- client/src/Redux/actions/productActions.js
+++ client/src/Redux/actions/productActions.js
@@ -19,12 +19,52 @@ export const getProductDataById = (id) => async (dispatch) => {
     }
 };
 
+// export const addToCart = (product, quantity, varient) => async (dispatch, getState) => {
+//     try {
+//         const { data } = await Api.postAddToCart(product, quantity, varient);
+//         console.log('data added to cart', data);
+//         dispatch({ type: ADD_TO_CART, payload: data });
+//     } catch (error) {
+//         console.log(error);
+//     }
+// };
+
 export const addToCart = (product, quantity, varient) => async (dispatch, getState) => {
-    try {
-        const { data } = await Api.postAddToCart(product, quantity, varient);
-        console.log('data added to cart', data);
-        dispatch({ type: ADD_TO_CART, payload: data });
-    } catch (error) {
-        console.log(error);
+    var cardItem = {
+        name: product?.name,
+        _id: product?._id,
+        image: product?.image,
+        varient: varient,
+        quantity: Number(quantity),
+        prices: product?.prices,
+        price: product?.prices[0][varient] * quantity
+    };
+
+    if (cardItem?.quantity > 10) {
+        alert('You cannot add more than 10 items');
+    } else {
+        if (cardItem?.quantity < 1) {
+            dispatch({
+                type: 'DELETE_FROM_CART',
+                payload: product
+            });
+        } else {
+            dispatch({
+                type: ADD_TO_CART,
+                payload: cardItem
+            });
+        }
     }
+    const cartItems = getState().cartReducer.cartItems;
+    localStorage.setItem('cartItems', JSON.stringify(cartItems));
+};
+
+export const deleteFromCart = (pizza) => async (dispatch, getState) => {
+    dispatch({
+        type: 'DELETE_FROM_CART',
+        payload: pizza
+    });
+
+    const cartItems = getState().cartReducer.cartItems;
+    localStorage.setItem('cartItems', JSON.stringify(cartItems));
 };
