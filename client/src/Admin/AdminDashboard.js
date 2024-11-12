1ed9190305cd2f1ed1ecb27c5afda7fe1a94784c
mukulsingh94868 <mukulsingh94868@gmail.com>
2024-11-12 16:48:04 +0530

    map component

--- client/src/Admin/AdminDashboard.js
+++ client/src/Admin/AdminDashboard.js
@@ -1,3 +1,4 @@
+import React, { useEffect, useState } from 'react';
 import BookmarkOutlinedIcon from '@mui/icons-material/BookmarkOutlined';
 import Button from '@mui/joy/Button';
 import Card from '@mui/joy/Card';
@@ -7,17 +8,16 @@ import CircularProgress from '@mui/joy/CircularProgress';
 import IconButton from '@mui/joy/IconButton';
 import SvgIcon from '@mui/joy/SvgIcon';
 import Typography from '@mui/joy/Typography';
-import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { getProductData } from '../Redux/actions/productActions';
 import { GetAllOrdersData } from '../Redux/actions/orderActions';
+import MapComponent from '../Compoments/MapComponent';
 
 const AdminDashboard = () => {
     const [data, setData] = useState([]);
 
     const dispatch = useDispatch();
-
     const prodData = useSelector((state) => state?.productReducer?.product);
     const orderState = useSelector((state) => state.orderReducer);
 
@@ -32,11 +32,36 @@ const AdminDashboard = () => {
         dispatch(GetAllOrdersData());
         dispatch(getProductData());
     }, [dispatch]);
+
+    const cardData = [
+        {
+            title: 'Total User',
+            count: data?.length,
+            progressValue: data?.length / 100,
+            link: '/dashboard/user-management',
+            buttonText: 'View User List'
+        },
+        {
+            title: 'Total Products',
+            count: prodData?.length,
+            progressValue: prodData?.length / 100,
+            link: '/dashboard/products-list',
+            buttonText: 'View Product List'
+        },
+        {
+            title: 'Total Orders',
+            count: orderState?.orders?.length,
+            progressValue: orderState?.orders?.length / 100,
+            link: '/dashboard/orders-list',
+            buttonText: 'View Order List'
+        }
+    ];
+
     return (
-        <>
-            <div className="Dashcontainer">
-                <div className="Dashcontainer1">
-                    <div className="layer layer-1">
+        <div className="Dashcontainer">
+            <div className="Dashcontainer1">
+                {cardData.map((card, index) => (
+                    <div className="layer layer-1" key={index}>
                         <Card variant="solid" color="primary" invertedColors>
                             <CardContent orientation="horizontal">
                                 <IconButton
@@ -46,7 +71,7 @@ const AdminDashboard = () => {
                                 >
                                     <BookmarkOutlinedIcon />
                                 </IconButton>
-                                <CircularProgress size="lg" determinate value={data?.length / 100}>
+                                <CircularProgress size="lg" determinate value={card.progressValue}>
                                     <SvgIcon>
                                         <svg
                                             xmlns="http://www.w3.org/2000/svg"
@@ -64,107 +89,27 @@ const AdminDashboard = () => {
                                     </SvgIcon>
                                 </CircularProgress>
                                 <CardContent>
-                                    <Typography level="body-md">Total User</Typography>
-                                    <Typography level="h2">{data?.length}</Typography>
+                                    <Typography level="body-md">{card.title}</Typography>
+                                    <Typography level="h2">{card.count}</Typography>
                                 </CardContent>
                             </CardContent>
                             <CardActions>
                                 <Button variant="solid" size="md">
-                                    <Link className='LinkColorLight' to="/dashboard/user-management"  >
-                                        View User List
+                                    <Link className="LinkColorLight" to={card.link}>
+                                        {card.buttonText}
                                     </Link>
                                 </Button>
                             </CardActions>
                         </Card>
                     </div>
+                ))}
 
-                    <div className="layer layer-1">
-                        <Card variant="solid" color="primary" invertedColors>
-                            <CardContent orientation="horizontal">
-                                <IconButton
-                                    variant="outlined"
-                                    size="sm"
-                                    sx={{ position: 'absolute', top: '0.75rem', right: '0.75rem' }}
-                                >
-                                    <BookmarkOutlinedIcon />
-                                </IconButton>
-                                <CircularProgress size="lg" determinate value={orderState?.orders?.length / 100}>
-                                    <SvgIcon>
-                                        <svg
-                                            xmlns="http://www.w3.org/2000/svg"
-                                            fill="none"
-                                            viewBox="0 0 24 24"
-                                            strokeWidth={1.5}
-                                            stroke="currentColor"
-                                        >
-                                            <path
-                                                strokeLinecap="round"
-                                                strokeLinejoin="round"
-                                                d="M2.25 18L9 11.25l4.306 4.307a11.95 11.95 0 015.814-5.519l2.74-1.22m0 0l-5.94-2.28m5.94 2.28l-2.28 5.941"
-                                            />
-                                        </svg>
-                                    </SvgIcon>
-                                </CircularProgress>
-                                <CardContent>
-                                    <Typography level="body-md">Total Products</Typography>
-                                    <Typography level="h2">{prodData?.length}</Typography>
-                                </CardContent>
-                            </CardContent>
-                            <CardActions>
-                                <Button variant="solid" size="md">
-                                    <Link className='LinkColorLight' to="/dashboard/products-list">
-                                        View Product List
-                                    </Link>
-                                </Button>
-                            </CardActions>
-                        </Card>
-                    </div>
-
-                    <div className="layer layer-1">
-                        <Card variant="solid" color="primary" invertedColors>
-                            <CardContent orientation="horizontal">
-                                <IconButton
-                                    variant="outlined"
-                                    size="sm"
-                                    sx={{ position: 'absolute', top: '0.75rem', right: '0.75rem' }}
-                                >
-                                    <BookmarkOutlinedIcon />
-                                </IconButton>
-                                <CircularProgress size="lg" determinate value={orderState?.orders?.length / 100}>
-                                    <SvgIcon>
-                                        <svg
-                                            xmlns="http://www.w3.org/2000/svg"
-                                            fill="none"
-                                            viewBox="0 0 24 24"
-                                            strokeWidth={1.5}
-                                            stroke="currentColor"
-                                        >
-                                            <path
-                                                strokeLinecap="round"
-                                                strokeLinejoin="round"
-                                                d="M2.25 18L9 11.25l4.306 4.307a11.95 11.95 0 015.814-5.519l2.74-1.22m0 0l-5.94-2.28m5.94 2.28l-2.28 5.941"
-                                            />
-                                        </svg>
-                                    </SvgIcon>
-                                </CircularProgress>
-                                <CardContent>
-                                    <Typography level="body-md">Total Orders</Typography>
-                                    <Typography level="h2">{orderState?.orders?.length}</Typography>
-                                </CardContent>
-                            </CardContent>
-                            <CardActions>
-                                <Button variant="solid" size="md">
-                                    <Link className='LinkColorLight' to="/dashboard/orders-list"  >
-                                        View Order List
-                                    </Link>
-                                </Button>
-                            </CardActions>
-                        </Card>
-                    </div>
+                <div className='layer layer-1'>
+                    <MapComponent />
                 </div>
             </div>
-        </>
-    )
-}
+        </div>
+    );
+};
 
 export default AdminDashboard;
---
+++ client/src/Compoments/MapComponent/index.js
@@ -0,0 +1,33 @@
+import React from "react";
+import WorldMap from "react-svg-worldmap";
+import './mapComponent.css';
+
+const data = [
+    { country: "cn", value: 1389618778 }, // china
+    { country: "in", value: 1311559204 }, // india
+    { country: "us", value: 331883986 }, // united states
+    { country: "id", value: 264935824 }, // indonesia
+    { country: "pk", value: 210797836 }, // pakistan
+    { country: "br", value: 210301591 }, // brazil
+    { country: "ng", value: 208679114 }, // nigeria
+    { country: "bd", value: 161062905 }, // bangladesh
+    { country: "ru", value: 141944641 }, // russia
+    { country: "mx", value: 127318112 }, // mexico
+];
+
+const MapComponent = () => {
+    return (
+        <>
+            <WorldMap
+                className="world-map"
+                color="red"
+                title="Top 10 Populous Countries"
+                value-suffix="people"
+                size="lg"
+                data={data}
+            />
+        </>
+    )
+};
+
+export default MapComponent;
