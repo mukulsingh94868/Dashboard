3e7a3f28a80b389ecf27d55d9e7198c0e36e01c5
mukulsingh94868 <mukulsingh94868@gmail.com>
2024-11-12 17:02:29 +0530

    refactor code

--- client/src/Admin/AdminDashboard.js
+++ client/src/Admin/AdminDashboard.js
@@ -1,4 +1,3 @@
-import React, { useEffect, useState } from 'react';
 import BookmarkOutlinedIcon from '@mui/icons-material/BookmarkOutlined';
 import Button from '@mui/joy/Button';
 import Card from '@mui/joy/Card';
@@ -8,16 +7,18 @@ import CircularProgress from '@mui/joy/CircularProgress';
 import IconButton from '@mui/joy/IconButton';
 import SvgIcon from '@mui/joy/SvgIcon';
 import Typography from '@mui/joy/Typography';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { getProductData } from '../Redux/actions/productActions';
 import { GetAllOrdersData } from '../Redux/actions/orderActions';
+import { Box, Grid } from '@mui/material';
 import MapComponent from '../Compoments/MapComponent';
 
 const AdminDashboard = () => {
     const [data, setData] = useState([]);
-
     const dispatch = useDispatch();
+
     const prodData = useSelector((state) => state?.productReducer?.product);
     const orderState = useSelector((state) => state.orderReducer);
 
@@ -33,35 +34,35 @@ const AdminDashboard = () => {
         dispatch(getProductData());
     }, [dispatch]);
 
-    const cardData = [
+    const cardInfo = [
         {
-            title: 'Total User',
-            count: data?.length,
+            title: "Total User",
+            value: data?.length,
             progressValue: data?.length / 100,
-            link: '/dashboard/user-management',
-            buttonText: 'View User List'
+            link: "/dashboard/user-management",
+            buttonText: "View User List"
         },
         {
-            title: 'Total Products',
-            count: prodData?.length,
+            title: "Total Products",
+            value: prodData?.length,
             progressValue: prodData?.length / 100,
-            link: '/dashboard/products-list',
-            buttonText: 'View Product List'
+            link: "/dashboard/products-list",
+            buttonText: "View Product List"
         },
         {
-            title: 'Total Orders',
-            count: orderState?.orders?.length,
+            title: "Total Orders",
+            value: orderState?.orders?.length,
             progressValue: orderState?.orders?.length / 100,
-            link: '/dashboard/orders-list',
-            buttonText: 'View Order List'
+            link: "/dashboard/orders-list",
+            buttonText: "View Order List"
         }
     ];
 
     return (
-        <div className="Dashcontainer">
-            <div className="Dashcontainer1">
-                {cardData.map((card, index) => (
-                    <div className="layer layer-1" key={index}>
+        <Box sx={{ width: '100%', padding: '20px' }}>
+            <Grid container spacing={3}>
+                {cardInfo.map((card, index) => (
+                    <Grid item xs={3} key={index}>
                         <Card variant="solid" color="primary" invertedColors>
                             <CardContent orientation="horizontal">
                                 <IconButton
@@ -90,25 +91,27 @@ const AdminDashboard = () => {
                                 </CircularProgress>
                                 <CardContent>
                                     <Typography level="body-md">{card.title}</Typography>
-                                    <Typography level="h2">{card.count}</Typography>
+                                    <Typography level="h2">{card.value}</Typography>
                                 </CardContent>
                             </CardContent>
                             <CardActions>
                                 <Button variant="solid" size="md">
-                                    <Link className="LinkColorLight" to={card.link}>
+                                    <Link className='LinkColorLight' to={card.link}>
                                         {card.buttonText}
                                     </Link>
                                 </Button>
                             </CardActions>
                         </Card>
-                    </div>
+                    </Grid>
                 ))}
+            </Grid>
 
-                <div className='layer layer-1'>
+            <Grid container spacing={3}>
+                <Grid item xs={12}>
                     <MapComponent />
-                </div>
-            </div>
-        </div>
+                </Grid>
+            </Grid>
+        </Box>
     );
 };
 
