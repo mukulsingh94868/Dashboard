447e900d50f6a5e55f2344551716bcbef941544e
mukulsingh94868 <mukulsingh94868@gmail.com>
2025-02-22 18:01:47 +0530

    change

--- server/server.js
+++ server/server.js
@@ -35,7 +35,6 @@ app.use('/api/categoryProduct', categoryProductRoutes);
 
 const PORT = 5000;
 const MONGO_URL = 'mongodb+srv://admin_dashboard:admin_dashboard_123@cluster0.t5q7pda.mongodb.net/admin_dashboard';
-
 mongoose.connect(MONGO_URL).then(() => {
     console.log('DB connection established');
     app.listen(PORT, () => {
