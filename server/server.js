5e58d9a4ca3e8d94f85032c9c21546a5b9b00148
mukulsingh94868 <mukulsingh94868@gmail.com>
2025-04-30 09:20:20 +0530

    save

--- server/server.js
+++ server/server.js
@@ -37,6 +37,7 @@ app.use('/api/contact', contactRoutes);
 
 const PORT = 5000;
 
+
 const MONGO_URL = 'mongodb+srv://admin_dashboard:admin_dashboard_123@cluster0.t5q7pda.mongodb.net/admin_dashboard';
 mongoose.connect(MONGO_URL).then(() => {
     console.log('DB connection established');
